344f4024bb743ef7f64e38b562671bd59a0dbeb8
jwhi <mynameisjeremywhite@gmail.com>
2026-05-03 22:17:53 -0500

    Add event name validation and prepare for implementing the heal event.

--- app/src/test/kotlin/AppTest.kt
+++ app/src/test/kotlin/AppTest.kt
@@ -1,6 +1,7 @@
 import com.jwhi.som.domains.evt.CompareType
 import com.jwhi.som.domains.evt.EvtOpDisplayMessage
 import com.jwhi.som.domains.evt.EvtOpIfMessage
+import com.jwhi.som.domains.evt.EvtOpUnimplemented
 import com.jwhi.som.domains.evt.TargetType
 import com.jwhi.som.domains.evt.TriggerType
 import com.jwhi.som.domains.reader.ExampleReader
@@ -17,8 +18,61 @@ class AppTest : FunSpec({
         val evt00Bytes = fileReader.binaryFileReader("/ExampleProject/DATA/MAP/00.evt")
         val evtEvents = evt00Bytes.toMapEvent()
 
-        test("Validate event length") {
+        test("Event length") {
             evtEvents.size shouldBe 251
+        }
+
+        test("Defined events") {
+            val definedEvents = evtEvents.filter {
+                it.definition.targetType != TargetType.NONE
+            }.map {
+                it.definition.name
+            }
+
+            definedEvents shouldBe listOf(
+                "CLOSE MAP",
+                "PLAYER DEATH",
+                "APPOINT",
+                "APPOINT",
+                "APPOINT",
+                "APPOINT",
+                "APPOINT",
+                "APPOINT",
+                "APPOINT",
+                "First Event",
+                "Man Response To Feeding Dwarf",
+                "Greeting before feeding dwarf",
+                "NEW EVENT",
+                "Talking",
+                "Rect Test 1",
+                "Circ Event 1",
+                "Heal Bump",
+                "Hurt Player",
+                "Chair Always On Return WhenLow",
+                "Timer Enable Bandit",
+                "Display Message",
+                "Display Message Formatted",
+                "Activate NPC (Bandit)",
+                "Activate Enemy (0002 Ooze)",
+                "Activate Item (0004 FirDagger)",
+                "Shop (5 Man's Shop)",
+                "WarpNPC2(x6z69x.1y.2z.5d120)",
+                "WarpEne2(x7z69x-.1y-.2z-.5d270",
+                "Terminate NPC (2 Bandit)",
+                "Terminate Enemy (2 Ooze)",
+                "Screen Effect Black Fades Off",
+                "Screen Effect Black Fades On",
+                "Screen Effect White Fades Off",
+                "Screen Effect White Fades On",
+                "Screen Effect Red",
+                "Screen Effect Red Loop",
+                "Screen Effect Green",
+                "Screen Effect Blue",
+                "DISPLAY BMP",
+                "IF PlayerHealthCounter > 25",
+                "IF Timer1 > 0 Otherwise",
+                "IF MESSAGE"
+            )
         }
 
         test("Validate display operation") {
@@ -77,6 +131,37 @@ class AppTest : FunSpec({
                     ifDisplayMessage.text shouldBe "IF message. First choise is True. \r\nSecond is False."
                     ifDisplayMessage.option1 shouldBe "True"
                     ifDisplayMessage.option2 shouldBe "False"
+                }
+            }
+        }
+
+        test("Heal player with checking counter") {
+            val healEvent = evtEvents[16]
+
+            withClue("Event Definition validation") {
+                assertSoftly(healEvent) {
+                    it.definition.name shouldBe "Heal Bump"
+                    it.definition.targetType shouldBe TargetType.NPC
+                    it.definition.targetId shouldBe 3
+                    it.definition.triggerType shouldBe TriggerType.APPROACH_CIRCLE
+                    it.definition.triggerItem shouldBe 0xFF.toUByte()
+                    it.definition.triggerCone shouldBe 360u
+                    it.definition.triggerRadius shouldBe 0.2f
+                    it.definition.condition.compareType shouldBe CompareType.NONE
+                    it.definition.pages.size shouldBe 1
+                }
+            }
+
+            withClue("PageOperation validation") {
+                assertSoftly(healEvent) {
+                    val pagePayloadOffset = it.definition.pages[0].payloadOffset
+                    pagePayloadOffset shouldBe 258872u
+
+                    it.pageOperations.size shouldBe 1
+                    it.pageOperations[pagePayloadOffset].shouldBeTypeOf<EvtOpUnimplemented>()
+                    val unimplementedMessage = it.pageOperations[pagePayloadOffset] as EvtOpUnimplemented
+                    unimplementedMessage.opId shouldBe 84u
+                    unimplementedMessage.opSize shouldBe 8u
                 }
             }
         }
